cb79aedbec319be3bc83c45cc98b8745c00aab0f
R. Kevin Nelson <kevin@rkn.la>
2015-02-18 17:13:22 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from os import path
+
+with open(path.join(path.dirname(__file__), 'README.md')) as readme:
+  long_description = readme.read()
+
+setup(
+  name="Pale",
+  version="0.1.0",
+  author="R. Kevin Nelson",
+  author_email="kevin@rkn.la",
+  description="Pale is a framework for crafting HTTP APIs.",
+  license="Copyright 2015-present, Loudr.fm. All rights reserved.",
+  url="https://github.com/Loudr/pale",
+  packages=['pale'],
+)
+
