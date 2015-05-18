68c7cb78eda0a63057de14b47814a867cf7adca8
R. Kevin Nelson <kevin@rkn.la>
2015-05-18 16:29:41 +0200

    Fix up IntegerField tests, use double quotes.
    Apostrophes are more common than quotations, so default to double-quotes
    (and try to be consistent)

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,6 +1,6 @@
 import unittest
 
-from pale.fields import BaseField, StringField
+from pale.fields import BaseField, IntegerField, StringField
 
 class FieldsTests(unittest.TestCase):
 
@@ -13,71 +13,66 @@ class FieldsTests(unittest.TestCase):
                 "This is a test field",
                 "This is where I put special notes about the field")
 
-        self.assertEqual(field.value_type, 'integer')
-        self.assertEqual(field.description, 'This is a test field')
+        self.assertEqual(field.value_type, "integer")
+        self.assertEqual(field.description, "This is a test field")
         self.assertEqual(field.details,
-                'This is where I put special notes about the field')
+                "This is where I put special notes about the field")
 
         doc = field.doc_dict()
-        self.assertEqual(doc['type'], 'integer')
-        self.assertEqual(doc['description'], 'This is a test field')
-        self.assertEqual(doc['extended_description'],
-                'This is where I put special notes about the field')
+        self.assertEqual(doc["type"], "integer")
+        self.assertEqual(doc["description"], "This is a test field")
+        self.assertEqual(doc["extended_description"],
+                "This is where I put special notes about the field")
 
         # test details default
-        field = BaseField('integer',
-                'This is a test field')
+        field = BaseField("integer",
+                "This is a test field")
         self.assertIsNone(field.details)
         doc = field.doc_dict()
-        self.assertIsNone(doc['extended_description'])
+        self.assertIsNone(doc["extended_description"])
 
 
     def test_create_string_field(self):
-        field = StringField('name',
-                'Your name',
+        field = StringField("Your name",
                 "You should expect a real name here, not some mish-mosh")
-        self.assertEqual(field.name, 'name')
-        self.assertEqual(field.value_type, 'string')
-        self.assertEqual(field.description, 'Your name')
+        self.assertEqual(field.value_type, "string")
+        self.assertEqual(field.description, "Your name")
         self.assertEqual(field.details,
                 "You should expect a real name here, not some mish-mosh")
 
         doc = field.doc_dict()
-        self.assertEqual(doc['name'], 'name')
-        self.assertEqual(doc['type'], 'string')
-        self.assertEqual(doc['description'], 'Your name')
-        self.assertEqual(doc['extended_description'],
+        self.assertEqual(doc["type"], "string")
+        self.assertEqual(doc["description"], "Your name")
+        self.assertEqual(doc["extended_description"],
                 "You should expect a real name here, not some mish-mosh")
 
-        field = StringField('name', 'Your name')
+        field = StringField("Your name")
         self.assertIsNone(field.details)
         doc = field.doc_dict()
-        self.assertIsNone(doc['extended_description'])
+        self.assertIsNone(doc["extended_description"])
 
 
     def test_integer_field(self):
-        field = IntegerField('amount',
-                'The amount of stuff for your things',
-                "This amount will always be a positive integer, but may be \
-only eventually consistent.")
+        field = IntegerField("The amount of stuff for your things",
+                "This amount will always be a positive integer, but may "
+                "be only eventually consistent.")
 
-        self.assertEqual(field.name, 'amount')
-        self.assertEqual(field.value_type, 'integer')
+        self.assertEqual(field.value_type, "integer")
         self.assertEqual(field.description,
-                'The amount of stuff for your things')
+                "The amount of stuff for your things")
         self.assertEqual(field.details,
-                "This amount will always be a positive integer, but may be \
-only eventually consistent.")
+                "This amount will always be a positive integer, but may "
+                "be only eventually consistent.")
 
         doc = field.doc_dict()
-        self.assertEqual(doc['name'], 'name')
-        self.assertEqual(doc['type'], 'string')
-        self.assertEqual(doc['description'], 'Your name')
-        self.assertEqual(doc['extended_description'],
-                "You should expect a real name here, not some mish-mosh")
+        self.assertEqual(doc["type"], "integer")
+        self.assertEqual(doc["description"],
+                "The amount of stuff for your things")
+        self.assertEqual(doc["extended_description"],
+                "This amount will always be a positive integer, but may "
+                "be only eventually consistent.")
 
-        field = IntegerField('amount', 'Some amount thing')
+        field = IntegerField("Some amount thing")
         self.assertIsNone(field.details)
         doc = field.doc_dict()
-        self.assertIsNone(doc['extended_description'])
-
+        self.assertIsNone(doc["extended_description"])
