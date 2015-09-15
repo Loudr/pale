09fe72fdb93d2d75fe267f1ce599502d5f59106d
R. Kevin Nelson <kevin@rkn.la>
2015-09-15 17:09:21 +0200

    Begin work on allowing 0 and 1 in boolean argument

--- pale/arguments/boolean.py
+++ pale/arguments/boolean.py
@@ -7,7 +7,7 @@ class BooleanArgument(BaseArgument):
         if item is None:
             item = self.default
 
-        if isinstance(item, (str, unicode)):
+        if isinstance(item, basestring):
             # coerce string types to bool, since we might get a string type
             # from the HTTP library
             val = item
@@ -15,6 +15,12 @@ class BooleanArgument(BaseArgument):
                 item = True
             elif val == 'false' or val == 'False':
                 item = False
+        elif isinstance(item, int):
+            val = item
+            if val == 0:
+                item = False
+            else:
+                item = True
 
         self._validate_type(item, item_name)
 
